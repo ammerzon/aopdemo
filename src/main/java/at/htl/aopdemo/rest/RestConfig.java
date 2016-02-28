c189901eeb3817ca9afc5376cfc1cde9e232707e
Simon Ammer @ MBP <simonammer@outlook.com>
2016-02-28 19:17:53 +0100

    Add cross origin resource sharing filter

--- src/main/java/at/htl/aopdemo/rest/RestConfig.java
+++ src/main/java/at/htl/aopdemo/rest/RestConfig.java
@@ -3,10 +3,6 @@ package at.htl.aopdemo.rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-/**
- * User: simonammer
- * Date: 28.02.16
- */
 @ApplicationPath("rs")
 public class RestConfig extends Application {
 
